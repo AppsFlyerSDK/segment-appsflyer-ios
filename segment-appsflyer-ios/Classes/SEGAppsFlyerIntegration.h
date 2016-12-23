48c0e3d4d440128f59b831e19251ba4756389be2
Andre Rabold <andre.rabold@gmail.com>
2016-12-23 12:00:35 -0800

    Compatibility with AppsFlyer SDK v4.5.12
    Seems the AppsFlyer SDK Version 4.5.12 has changed include paths from `AppsFlyerTracker/AppsFlyerTracker.h` to `AppsFlyerLib/AppsFlyerTracker`. This patch fixes the Segment integration.

--- segment-appsflyer-ios/Classes/SEGAppsFlyerIntegration.h
+++ segment-appsflyer-ios/Classes/SEGAppsFlyerIntegration.h
@@ -8,7 +8,7 @@
 
 #import <Foundation/Foundation.h>
 #import <Analytics/SEGAnalytics.h>
-#import <AppsFlyerTracker/AppsFlyerTracker.h>
+#import <AppsFlyerLib/AppsFlyerTracker.h>
 
 @interface SEGAppsFlyerIntegration : NSObject <SEGIntegration, AppsFlyerTrackerDelegate>
 
