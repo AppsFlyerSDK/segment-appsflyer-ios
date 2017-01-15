f3c95bc163bc8da7d7a624d638b63180e97a3cd1
Golan <golan@Golans-MacBook-Pro.local>
2017-01-15 15:07:19 +0200

    Revert "check import"
    This reverts commit af43181ee37901b30f16b3e5d8eecdb9c177ee67.

--- segment-appsflyer-ios/Classes/SEGAppsFlyerIntegration.h
+++ segment-appsflyer-ios/Classes/SEGAppsFlyerIntegration.h
@@ -8,7 +8,7 @@
 
 #import <Foundation/Foundation.h>
 #import <Analytics/SEGAnalytics.h>
-#import <AppsFlyerDynamicFramework/AppsFlyerTracker.h>
+#import <AppsFlyerTracker/AppsFlyerTracker.h>
 
 @interface SEGAppsFlyerIntegration : NSObject <SEGIntegration, AppsFlyerTrackerDelegate>
 
