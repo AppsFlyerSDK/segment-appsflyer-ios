3e289efe33860b67e6c6f9171ee4d180e6143a3c
Evan Coleman <e@edc.me>
2019-09-12 15:18:40 -0400

    Fix headers

--- segment-appsflyer-ios/Classes/SegmentAppsFlyeriOS.h
+++ segment-appsflyer-ios/Classes/SegmentAppsFlyeriOS.h
@@ -16,4 +16,4 @@ FOUNDATION_EXPORT const unsigned char SegmentAppsFlyeriOSVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <SegmentAppsFlyeriOS/PublicHeader.h>
 
-
+#import <SegmentAppsFlyeriOS/SEGAppsFlyerIntegrationFactory.h>
