e2993bdcd3091072c7a33cbc5b40e944b87beb2d
Andrii Hahan <andrii.h@appsflyer.com>
2020-01-20 14:28:08 +0200

    Carthage example

---
+++ examples/ios-objc-carthage/ios-objc-carthage/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ios-objc-carthage
+//
+//  Created by Andrii Hahan on 20.01.2020.
+//  Copyright © 2020 Andrii Hahan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ examples/ios-objc-carthage/ios-objc-carthage/AppDelegate.m
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.m
+//  ios-objc-carthage
+//
+//  Created by Andrii Hahan on 20.01.2020.
+//  Copyright © 2020 Andrii Hahan. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // Override point for customization after application launch.
+    return YES;
+}
+
+@end
