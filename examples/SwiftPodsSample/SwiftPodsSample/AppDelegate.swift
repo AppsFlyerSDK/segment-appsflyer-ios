f7543b247c72755be84b0388181ed3c82f469d0d
af-vs <vitaly.sokolov@appsflyer.com>
2020-08-12 15:33:19 +0300

    Add new CocoaPods-Swift Sample App

---
+++ examples/SwiftPodsSample/SwiftPodsSample/AppDelegate.swift
@@ -0,0 +1,86 @@
+//
+//  AppDelegate.swift
+//  SwiftPodsSample
+//
+//  Created by Vitaly Sokolov on 12.08.2020.
+//
+
+import UIKit
+import Analytics
+import AppsFlyerLib
+import AppTrackingTransparency
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        // For AppsFLyer debug logs uncomment the line below
+//        AppsFlyerLib.shared().isDebug = true
+        
+        AppsFlyerLib.shared().waitForAdvertisingIdentifier(withTimeoutInterval: 60)
+        ATTrackingManager.requestTrackingAuthorization(completionHandler: { (status) in
+            // ...
+        })
+
+        /*
+         Based on your needs you can either pass a delegate to process deferred
+         and direct deeplinking callbacks or disregard them.
+         If you choose to use the delegate, see extension to this class below
+         */
+        let factoryWithDelegate : SEGAppsFlyerIntegrationFactory = SEGAppsFlyerIntegrationFactory.create(withLaunch: self)
+//        let factoryNoDelegate = SEGAppsFlyerIntegrationFactory()
+        
+        // Segment initialization
+        let config = AnalyticsConfiguration(writeKey: "WYsuyFINOKZuQyQAGn5JQoCgIdhOI146")
+        config.use(factoryWithDelegate)
+        //      config.use(factoryNoDelegate)
+        config.enableAdvertisingTracking = true       //OPTIONAL
+        config.trackApplicationLifecycleEvents = true //OPTIONAL
+        config.trackDeepLinks = true                  //OPTIONAL
+        config.trackPushNotifications = true          //OPTIONAL
+        config.trackAttributionData = true            //OPTIONAL
+        
+        Analytics.debug(false)
+        Analytics.setup(with: config)
+        return true
+    }
+    
+    func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
+        AppsFlyerLib.shared().continue(userActivity, restorationHandler: nil)
+        return true
+    }
+    
+    func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([Any]?) -> Void) -> Bool {
+        AppsFlyerLib.shared().continue(userActivity, restorationHandler: restorationHandler)
+        return true
+    }
+
+    func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+        AppsFlyerLib.shared().handleOpen(url, sourceApplication: sourceApplication, withAnnotation: annotation)
+        return true
+    }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        AppsFlyerLib.shared().handleOpen(url, options: options)
+        return true
+    }
+}
+
+extension AppDelegate: SEGAppsFlyerLibDelegate {
+    func onConversionDataSuccess(_ conversionInfo: [AnyHashable : Any]) {
+        print("GCD: " + conversionInfo.description)
+    }
+    
+    func onAppOpenAttribution(_ attributionData: [AnyHashable : Any]) {
+        print("OAOA: " + attributionData.description)
+    }
+    func onConversionDataFail(_ error: Error) {
+        print("\(error)")
+    }
+    
+    func onAppOpenAttributionFailure(_ error: Error) {
+        print("\(error)")
+    }
+}
+
---
+++ examples/SwiftPodsSample/SwiftPodsSample/SEGAppsFlyer-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  SEGAppsFlyer-Bridging-Header.h
+//  SwiftPodsSample
+//
+//  Created by Vitaly Sokolov on 12.08.2020.
+//
+
+#ifndef SEGAppsFlyer_Bridging_Header_h
+#define SEGAppsFlyer_Bridging_Header_h
+
+#import "SEGAppsFlyerIntegrationFactory.h"
+
+#endif /* SEGAppsFlyer_Bridging_Header_h */
