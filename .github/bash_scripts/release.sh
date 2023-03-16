b4976194bd741cce4009d16e9433ec40179d4578
Moris Gateno <moris.gateno@appsflyer.com>
2023-03-16 12:11:11 +0200

    release bash file

--- .github/bash_scripts/release.sh
+++ .github/bash_scripts/release.sh
@@ -8,12 +8,16 @@ sed -r -i '' "s/version_release = \'[0-9]+\.[0-9]+\.[0-9]+\'/version_release = \
 sed -r -i '' "s/(.*pod \'segment-appsflyer-ios\')(.*)/\1,\'$releaseversion\'/g" examples/SwiftPodsSample/Podfile
 sed -r -i '' "s/(.*pod \'segment-appsflyer-ios\')(.*)/\1,\'$releaseversion\'/g" examples/ObjcPodsSample/Podfile
 
-desctiption_to_release_notes=$(cat "releasenotes.$releaseversion")
-sed -i.bak "/# Release Notes/a \\
-\\
-### $releaseversion\\
-* Updated iOS SDK to v$releaseversion\\
-$desctiption_to_release_notes\\
-" RELEASENOTES.md
+# desctiption_to_release_notes=$(cat "releasenotes.$releaseversion")
+# sed -i.bak "/# Release Notes/a \\
+# \\
+# ### $releaseversion\\
+# * Updated iOS SDK to v$releaseversion\\
+# $desctiption_to_release_notes\\
+# " RELEASENOTES.md
 
-rm -r RELEASENOTES.md.bak
+# rm -r RELEASENOTES.md.bak
+sed -i '' 's/^/* /' "releasenotes.$releaseversion"
+NEW_VERSION_RELEASE_NOTES=$(cat "releasenotes.$releaseversion")
+NEW_VERSION_SECTION="### $releaseversion\n \n$NEW_VERSION_RELEASE_NOTES\n\n"
+echo -e "$NEW_VERSION_SECTION$(cat RELEASENOTES.md)" > RELEASENOTES.md
