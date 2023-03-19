3ad71f93fa3bdb2ca12677423a50f972f0469d99
Moris Gateno <moris.gateno@appsflyer.com>
2023-03-19 13:32:13 +0200

    yml changes after Amit's review

--- .github/bash_scripts/pre_release.sh
+++ .github/bash_scripts/pre_release.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-releaseversion=$1
+appsflyerlibversion=$1
 
-sed -i '' "s/version_pre_release = \'.*\'/version_pre_release = \'$releaseversion\'/g" segment-appsflyer-ios.podspec
+sed -i '' "s/version_appsflyerLib = \'.*\'/version_appsflyerLib = \'$appsflyerlibversion\'/g" segment-appsflyer-ios.podspec
 
-sed -r -i '' "s/(.*AppsFlyerLib.*)([0-9]+\.[0-9]+\.[0-9]+)(.*)/\1$releaseversion\3/g" Package.swift
+sed -r -i '' "s/(.*AppsFlyerLib.*)([0-9]+\.[0-9]+\.[0-9]+)(.*)/\1$appsflyerlibversion\3/g" Package.swift
 
 sed -r -i '' "s/(.*pod \'segment-appsflyer-ios\')(.*\'[0-9]+\.[0-9]+\.[0-9]+\')/\1,:podspec => \'\.\.\/\.\.\/segment-appsflyer-ios.podspec\'/g" examples/SwiftPodsSample/Podfile
 sed -r -i '' "s/(.*pod \'segment-appsflyer-ios\')(.*\'[0-9]+\.[0-9]+\.[0-9]+\')/\1,:podspec => \'\.\.\/\.\.\/segment-appsflyer-ios.podspec\'/g" examples/ObjcPodsSample/Podfile
 
-sed -r -i '' "s/(## This is a Segment wrapper for AppsFlyer SDK that is built with iOS SDK v)(.*)/\1$releaseversion./g" README.md
-sed -r -i '' "s/(.*pod \'segment-appsflyer-ios.*)([0-9]+\.[0-9]+\.[0-9]+)(.*)/\1$releaseversion\3/g" README.md
+sed -r -i '' "s/(## This is a Segment wrapper for AppsFlyer SDK that is built with iOS SDK v)(.*)/\1$appsflyerlibversion./g" README.md
+sed -r -i '' "s/(.*pod \'segment-appsflyer-ios.*)([0-9]+\.[0-9]+\.[0-9]+)(.*)/\1$appsflyerlibversion\3/g" README.md
 
-touch "releasenotes.$releaseversion"
+touch "releasenotes.$appsflyerlibversion"
--- segment-appsflyer-ios.podspec
+++ segment-appsflyer-ios.podspec
@@ -1,9 +1,9 @@
-version_pre_release = '6.9.1'
-version_release = '6.9.1'
+version_appsflyerLib = '6.9.1'
+version_plugin = '6.9.1'
 
 Pod::Spec.new do |s|
   s.name             = "segment-appsflyer-ios"
-  s.version          = version_release
+  s.version          = version_plugin
   s.summary          = "AppsFlyer Integration for Segment's analytics-ios library."
 
   s.description      = <<-DESC
@@ -25,14 +25,14 @@ Pod::Spec.new do |s|
 
   s.default_subspecs = 'Main'
   s.subspec 'Main' do |ss|
-    ss.ios.dependency 'AppsFlyerFramework',version_pre_release
-    ss.tvos.dependency 'AppsFlyerFramework',version_pre_release
+    ss.ios.dependency 'AppsFlyerFramework',version_appsflyerLib
+    ss.tvos.dependency 'AppsFlyerFramework',version_appsflyerLib
     ss.source_files = 'segment-appsflyer-ios/Classes/**/*'
   end
   
   s.subspec 'Strict' do |ss|
-    ss.ios.dependency 'AppsFlyerFramework/Strict',version_pre_release
-    ss.tvos.dependency 'AppsFlyerFramework/Strict',version_pre_release
+    ss.ios.dependency 'AppsFlyerFramework/Strict',version_appsflyerLib
+    ss.tvos.dependency 'AppsFlyerFramework/Strict',version_appsflyerLib
     ss.source_files = 'segment-appsflyer-ios/Classes/**/*'
     end
 end 
