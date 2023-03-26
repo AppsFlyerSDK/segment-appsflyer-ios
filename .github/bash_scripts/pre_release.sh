8c6b5fd1ec63a0863f836af498e1f4aaab98cfdf
Moris Gateno <moris.gateno@appsflyer.com>
2023-03-26 09:17:26 +0300

    camel case in scripts

--- .github/bash_scripts/pre_release.sh
+++ .github/bash_scripts/pre_release.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-appsflyerlibversion=$1
+appsflyerLibVersion=$1
 
-sed -i '' "s/version_appsflyerLib = \'.*\'/version_appsflyerLib = \'$appsflyerlibversion\'/g" segment-appsflyer-ios.podspec
+sed -i '' "s/version_appsflyerLib = \'.*\'/version_appsflyerLib = \'$appsflyerLibVersion\'/g" segment-appsflyer-ios.podspec
 
-sed -r -i '' "s/(.*AppsFlyerLib.*)([0-9]+\.[0-9]+\.[0-9]+)(.*)/\1$appsflyerlibversion\3/g" Package.swift
+sed -r -i '' "s/(.*AppsFlyerLib.*)([0-9]+\.[0-9]+\.[0-9]+)(.*)/\1$appsflyerLibVersion\3/g" Package.swift
 
 sed -r -i '' "s/(.*pod \'segment-appsflyer-ios\')(.*\'[0-9]+\.[0-9]+\.[0-9]+\')/\1,:podspec => \'\.\.\/\.\.\/segment-appsflyer-ios.podspec\'/g" examples/SwiftPodsSample/Podfile
 sed -r -i '' "s/(.*pod \'segment-appsflyer-ios\')(.*\'[0-9]+\.[0-9]+\.[0-9]+\')/\1,:podspec => \'\.\.\/\.\.\/segment-appsflyer-ios.podspec\'/g" examples/ObjcPodsSample/Podfile
 
-sed -r -i '' "s/(## This is a Segment wrapper for AppsFlyer SDK that is built with iOS SDK v)(.*)/\1$appsflyerlibversion./g" README.md
-sed -r -i '' "s/(.*pod \'segment-appsflyer-ios.*)([0-9]+\.[0-9]+\.[0-9]+)(.*)/\1$appsflyerlibversion\3/g" README.md
+sed -r -i '' "s/(## This is a Segment wrapper for AppsFlyer SDK that is built with iOS SDK v)(.*)/\1$appsflyerLibVersion./g" README.md
+sed -r -i '' "s/(.*pod \'segment-appsflyer-ios.*)([0-9]+\.[0-9]+\.[0-9]+)(.*)/\1$appsflyerLibVersion\3/g" README.md
 
-touch "releasenotes.$appsflyerlibversion"
+touch "releasenotes.$appsflyerLibVersion"
--- .github/bash_scripts/release.sh
+++ .github/bash_scripts/release.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-releaseversion=$1
+releaseVersion=$1
 
-sed -r -i '' "s/version_plugin = \'[0-9]+\.[0-9]+\.[0-9]+\'/version_plugin = \'$releaseversion\'/g" segment-appsflyer-ios.podspec
+sed -r -i '' "s/version_plugin = \'[0-9]+\.[0-9]+\.[0-9]+\'/version_plugin = \'$releaseVersion\'/g" segment-appsflyer-ios.podspec
 
-sed -r -i '' "s/(.*pod \'segment-appsflyer-ios\')(.*)/\1,\'$releaseversion\'/g" examples/SwiftPodsSample/Podfile
-sed -r -i '' "s/(.*pod \'segment-appsflyer-ios\')(.*)/\1,\'$releaseversion\'/g" examples/ObjcPodsSample/Podfile
+sed -r -i '' "s/(.*pod \'segment-appsflyer-ios\')(.*)/\1,\'$releaseVersion\'/g" examples/SwiftPodsSample/Podfile
+sed -r -i '' "s/(.*pod \'segment-appsflyer-ios\')(.*)/\1,\'$releaseVersion\'/g" examples/ObjcPodsSample/Podfile
 
-sed -i '' 's/^/* /' "releasenotes.$releaseversion"
-NEW_VERSION_RELEASE_NOTES=$(cat "releasenotes.$releaseversion")
-NEW_VERSION_SECTION="### $releaseversion\n$NEW_VERSION_RELEASE_NOTES\n\n"
+sed -i '' 's/^/* /' "releasenotes.$releaseVersion"
+NEW_VERSION_RELEASE_NOTES=$(cat "releasenotes.$releaseVersion")
+NEW_VERSION_SECTION="### $releaseVersion\n$NEW_VERSION_RELEASE_NOTES\n\n"
 echo -e "$NEW_VERSION_SECTION$(cat RELEASENOTES.md)" > RELEASENOTES.md
 
-rm -r "releasenotes.$releaseversion"
+rm -r "releasenotes.$releaseVersion"
