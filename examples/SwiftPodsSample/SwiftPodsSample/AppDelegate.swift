0757c0e810fa56f7003a69a7a622b1199b9c65b2
Moris Gateno <moris.gateno@appsflyer.com>
2024-06-10 12:16:40 +0300

    .

--- examples/SwiftPodsSample/SwiftPodsSample/AppDelegate.swift
+++ examples/SwiftPodsSample/SwiftPodsSample/AppDelegate.swift
@@ -29,7 +29,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
       // let factoryNoDelegate = SEGAppsFlyerIntegrationFactory()
         
         // Segment initialization
-        let config = AnalyticsConfiguration(writeKey: "SEGMENT_KEYY")
+        let config = AnalyticsConfiguration(writeKey: "SEGMENT_KEYYy")
         config.use(factoryWithDelegate)
         //      config.use(factoryNoDelegate)
         config.enableAdvertisingTracking = true       //OPTIONAL
