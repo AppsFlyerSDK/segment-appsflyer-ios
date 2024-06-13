48bdae9fc0c60867e7f96a67e7a88de61660c1b5
Moris Gateno <moris.gateno@appsflyer.com>
2024-06-13 15:22:17 +0300

    .

--- .github/bash_scripts/pre_release.sh
+++ .github/bash_scripts/pre_release.sh
@@ -10,8 +10,8 @@ mv segment-appsflyer-ios.podspec segment-appsflyer-ios-qa.podspec
 
 sed -r -i '' "s/(.*AppsFlyerLib.*)([0-9]+\.[0-9]+\.[0-9]+)(.*)/\1$appsflyerLibVersion\3/g" Package.swift
 
-sed -r -i '' "s/(.*pod \'segment-appsflyer-ios\')(.*\'[0-9]+\.[0-9]+\.[0-9]+\')/\1,\'$rcVersion\'/g" examples/SwiftPodsSample/Podfile
-sed -r -i '' "s/(.*pod \'segment-appsflyer-ios\')(.*\'[0-9]+\.[0-9]+\.[0-9]+\')/\1,\'$rcVersion\'/g" examples/ObjcPodsSample/Podfile
+sed -r -i '' "s/(.*pod \'segment-appsflyer-ios)\'(.*\'[0-9]+\.[0-9]+\.[0-9]+\')/\1\',\'$rcVersion\'/g" examples/SwiftPodsSample/Podfile
+sed -r -i '' "s/(.*pod \'segment-appsflyer-ios)\'(.*\'[0-9]+\.[0-9]+\.[0-9]+\')/\1\',\'$rcVersion\'/g" examples/ObjcPodsSample/Podfile
 
 sed -r -i '' "s/(## This is a Segment wrapper for AppsFlyer SDK that is built with iOS SDK v)(.*)/\1$appsflyerLibVersion./g" README.md
 sed -r -i '' "s/(.*pod \'segment-appsflyer-ios.*)([0-9]+\.[0-9]+\.[0-9]+)(.*)/\1$appsflyerLibVersion\3/g" README.md
