0e987e211034e79dcd4895e1ba0ae315157d1748
AmitLY21 <Amit.Levy@appsflyer.com>
2025-12-09 14:50:00 +0200

    Fix CI Swift linking

--- .github/bash_scripts/podfile_creation.sh
+++ .github/bash_scripts/podfile_creation.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-echo "# Uncomment the next line to define a global platform for your project
-# platform :ios, '9.0'
+echo "platform :ios, '12.0'
 
 target 'segment-appsflyer-ios' do
   # Comment the next line if you don't want to use dynamic frameworks
@@ -19,5 +18,14 @@ target 'SegmentAppsFlyeriOSTests' do
   # Pods for SegmentAppsFlyeriOSTests
   pod 'OCMock'
 
+end
+
+# Post-install hook to fix Swift linking issues with AppsFlyer SDK
+post_install do |installer|
+  installer.pods_project.targets.each do |target|
+    target.build_configurations.each do |config|
+      config.build_settings['BUILD_LIBRARY_FOR_DISTRIBUTION'] = 'YES'
+    end
+  end
 end" > Podfile
 
