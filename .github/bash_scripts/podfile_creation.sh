51314e8fd8a48b331a17d56833933ced5eda39cb
AmitLY21 <Amit.Levy@appsflyer.com>
2025-12-09 15:05:34 +0200

    CI Tests fix

--- .github/bash_scripts/podfile_creation.sh
+++ .github/bash_scripts/podfile_creation.sh
@@ -27,5 +27,27 @@ post_install do |installer|
       config.build_settings['BUILD_LIBRARY_FOR_DISTRIBUTION'] = 'YES'
     end
   end
+  
+  # Configure main project for Swift framework compatibility
+  installer.aggregate_targets.each do |aggregate_target|
+    aggregate_target.user_project.native_targets.each do |target|
+      target.build_configurations.each do |config|
+        if target.name == 'segment-appsflyer-ios'
+          # Make missing Swift compatibility libraries non-fatal
+          # These are backward compatibility shims removed in Xcode 16+
+          config.build_settings['OTHER_LDFLAGS'] ||= ['\$(inherited)']
+          # Use -weak-l prefix to make these libraries optional
+          config.build_settings['OTHER_LDFLAGS'] << '-Wl,-weak-lswiftCompatibility56'
+          config.build_settings['OTHER_LDFLAGS'] << '-Wl,-weak-lswiftCompatibilityConcurrency'
+          config.build_settings['OTHER_LDFLAGS'] << '-Wl,-weak-lswiftCompatibilityPacks'
+          # Suppress linker warnings
+          config.build_settings['OTHER_LDFLAGS'] << '-Wl,-w'
+        end
+        if target.name == 'SegmentAppsFlyeriOSTests'
+          config.build_settings['ALWAYS_EMBED_SWIFT_STANDARD_LIBRARIES'] = 'YES'
+        end
+      end
+    end
+  end
 end" > Podfile
 
