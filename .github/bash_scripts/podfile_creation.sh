3ac5aea5219b3de9215e15bc00e69c6101161294
AmitLY21 <Amit.Levy@appsflyer.com>
2025-12-09 14:55:41 +0200

    Fix CI Swift linking errors

--- .github/bash_scripts/podfile_creation.sh
+++ .github/bash_scripts/podfile_creation.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 echo "platform :ios, '12.0'
+use_frameworks!
 
 target 'segment-appsflyer-ios' do
   # Comment the next line if you don't want to use dynamic frameworks
-  use_frameworks!
-
   # Pods for segment-appsflyer-ios
   pod 'Analytics'
   pod 'AppsFlyerFramework'
@@ -13,8 +12,6 @@ end
 
 target 'SegmentAppsFlyeriOSTests' do
   # Comment the next line if you don't want to use dynamic frameworks
-  use_frameworks!
-
   # Pods for SegmentAppsFlyeriOSTests
   pod 'OCMock'
 
@@ -25,6 +22,18 @@ post_install do |installer|
   installer.pods_project.targets.each do |target|
     target.build_configurations.each do |config|
       config.build_settings['BUILD_LIBRARY_FOR_DISTRIBUTION'] = 'YES'
+    end
+  end
+  
+  # Configure project targets for Swift framework compatibility
+  installer.aggregate_targets.each do |aggregate_target|
+    aggregate_target.user_project.native_targets.each do |target|
+      target.build_configurations.each do |config|
+        if target.name == 'SegmentAppsFlyeriOSTests'
+          # Test bundle MUST embed Swift standard libraries
+          config.build_settings['ALWAYS_EMBED_SWIFT_STANDARD_LIBRARIES'] = 'YES'
+        end
+      end
     end
   end
 end" > Podfile
